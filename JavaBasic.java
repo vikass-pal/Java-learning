be1bf0e2e923806f8606a985101140aabfa4ce0f
Vikas Pal <vikas.pal_2022@ltce.in>
2025-05-30 22:06:33 +0530

    javabasic2

--- JavaBasic.java
+++ JavaBasic.java
@@ -1,5 +1,7 @@
 public class JavaBasic {
     public static void main(String args[]) {
-        
+        System.out.println("Hello World!");
+
+        System.out.print("Hello vikas!");
     }
-}
+} 
