f30a1c1fac9d29d75a43ce30860e77a68aaceeaf
Vikas Pal <vikas.pal_2022@ltce.in>
2025-05-31 20:32:06 +0530

    Variables

---
+++ VariablesinJava.java
@@ -0,0 +1,10 @@
+public class VariablesinJava{
+    public static void main(String args[]) {
+        int a = 10;
+        int b= 20;
+        String name = "tony Montana";
+        System.out.println(a);
+        System.out.println(b);
+        System.out.println(name);
+    }
+}
