b2fdd1688c4fd100453598a5ca0717c643a3f259
Vikas Pal <vikas.pal_2022@ltce.in>
2025-05-31 20:48:40 +0530

    DataTypes

---
+++ DataTypes.java
@@ -0,0 +1,13 @@
+public class DataTypes {
+    public static void main(String args[]) {
+        byte b = 8;
+        char ch='a';
+        boolean var = true;
+        float price = 10.5f;
+        int num = 25;
+        // long
+        // double
+        short n = 240;
+
+    }
+} 
