c20fd5d00f1a78fff90efeb12a6573326f5235ab
Vikas Pal <vikas.pal_2022@ltce.in>
2025-05-31 22:48:50 +0530

    inputsinjava

---
+++ javainput.java
@@ -0,0 +1,25 @@
+ import java.util.*;
+
+public class javainput {
+    public static void main (String args[]){
+        Scanner sc = new Scanner(System.in);
+        // String input = sc.next();
+        // System.out.println(input);
+
+
+        // String name = sc.nextLine();
+        // System.out.println(name);
+
+        // int number = sc.nextInt(); 
+        // System.out.println(number);
+
+        // add two numbers with input
+
+        int a = sc.nextInt();
+        int b = sc.nextInt();
+
+        int sum = a+b;
+        System.out.println(sum);
+    
+} 
+}
