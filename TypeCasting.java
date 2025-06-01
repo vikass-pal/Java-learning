a397b23467a138455e77edbb6b603fb7c859ad33
Vikas Pal <vikas.pal_2022@ltce.in>
2025-06-01 20:51:12 +0530

    TypeCasting

---
+++ TypeCasting.java
@@ -0,0 +1,10 @@
+import java.util.*;
+
+ public class TypeCasting {
+    public static void main(String args []) {
+        Scanner sc = new Scanner(System.in);
+        float f = 25.0f;
+        int b= (int)f;
+        System.out.println(b);
+    }
+}
