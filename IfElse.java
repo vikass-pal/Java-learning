6fed626b57eede095ff274156d5e01b1f5c8598d
Vikas Pal <vikas.pal_2022@ltce.in>
2025-06-07 16:10:51 +0530

    IFElse

---
+++ IfElse.java
@@ -0,0 +1,52 @@
+import java.util.*;
+public class IfElse {
+     public static void main(String args []) {
+
+    //   Exampl of ifelse by age
+        // Scanner sc = new Scanner(System.in);
+
+        // int age = sc.nextInt();
+
+        // if (age >= 18) {
+        //     System.out.println(" youre an adult");
+        // }
+        // if(age>13 && age < 18) {
+        //     System.out.println("Youre a teenager");
+        // }  
+        // else {
+        //     System.out.println(" youre a minor");
+        // }
+
+        
+// example of printing the largest of 2 numbers
+
+        Scanner sc = new Scanner(System.in);
+        // System.out.println("Enter value of A");
+        // int A = sc.nextInt();
+        // System.out.println("Enter value of B");
+        // int B = sc.nextInt();
+
+        // if(A>=B) {
+        //     System.out.println("A is greater" + A);
+        // }
+        // else {
+        //     System.out.println("B is greater" + B);
+        // }
+
+        // example of EVEn or Odd 
+
+int num = sc.nextInt();
+if (num % 2 == 0) {
+    System.out.println("ITS EVEN");
+}
+else {
+    System.out.println("ITS ODD");
+}
+     }
+}
+
+
+
+
+
+
