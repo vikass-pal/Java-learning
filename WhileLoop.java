dddcf3bb340cc530f7e5fce2aa35e30438e915e2
Vikas Pal <vikas.pal_2022@ltce.in>
2025-06-08 18:22:06 +0530

    while

---
+++ WhileLoop.java
@@ -0,0 +1,11 @@
+public class WhileLoop {
+    public static void main(String[] args) {
+       int i = 0;
+       while(i<50) {
+        System.out.println("vkas");
+        i++;
+       }
+       System.out.println("printed 30 times");
+    }
+    
+}
