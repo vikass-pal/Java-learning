5aaf9cc2bb78a06ed5280868637a9f0b649300bf
Vikas Pal <vikas.pal_2022@ltce.in>
2025-06-08 12:42:02 +0530

    switch

---
+++ Switch.java
@@ -0,0 +1,16 @@
+public class Switch {
+    public static void main (String args[]) {
+        int number = 2;
+
+        switch(number) {
+            case 1: System.out.println("1");
+            break;
+             case 2: System.out.println("2");
+            break;
+             case 3: System.out.println("3");
+            break;
+            default: System.out.println("Not 1, 2 or 3");
+
+        }
+    }
+}
