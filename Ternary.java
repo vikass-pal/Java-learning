8f37eb5d1b00b81b8fb2816e29b521eb2b23c15d
Vikas Pal <vikas.pal_2022@ltce.in>
2025-06-08 11:13:44 +0530

    TernaryExample

--- Ternary.java
+++ Ternary.java
@@ -1,9 +1,17 @@
+
+import java.util.*;
 public class Ternary {
     public static void main (String args[]) {
-        int number = 4;
+        // int number = 4;
 
-        // ternary operator
-        String num = ((number %2) == 0) ? "EvEn" : "Odd";
-        System.out.println(num);
+        // // ternary operator
+        // String num = ((number %2) == 0) ? "EvEn" : "Odd";
+        // System.out.println(num);
+
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter a number:");
+        int marks = sc.nextInt();
+        String result = (marks >=33) ? "Pass": "Fail";
+        System.out.println(result);
     }
 }
