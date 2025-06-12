b0932f0ec6290fa1cbb63eab8f3cf344c444f63b
Vikas Pal <vikas.pal_2022@ltce.in>
2025-06-12 21:15:46 +0530

    function overloading

---
+++ 11_Functions/functionOverloadingexamp.java
@@ -0,0 +1,15 @@
+public class functionOverloadingexamp {
+    
+    public static int sum(int a, int b) {
+        return a+b;
+    }
+    
+     public static int sum(int a, int b, int c) {
+        return a+b+c;
+    }
+    
+    public static void main(String[] args) {
+       System.out.println(sum(2,3,4));
+        System.out.println(sum(2,3));
+    }
+} 
