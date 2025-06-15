6249eca61294af39c423a17b0014efd71696f950
Vikas Pal <vikas.pal_2022@ltce.in>
2025-06-15 20:58:00 +0530

    butterfly pattern

---
+++ numberpyramid.java
@@ -0,0 +1,64 @@
+// public class numberpyramid {
+
+//     public static void numberpy(int n) {
+//         for(int i=1; i<=n; i++) {
+//             // for spaces
+//             for(int j=1; j<=n-i; j++) {
+//                 System.out.print(" ");
+//             }
+//             for(int j=1; j<=i; j++) {
+//                 System.out.print(i+" ");
+//             }
+//             System.out.println();
+//         }
+//     }
+
+//     public static void main(String[] args) {
+//         numberpy(5);
+//     }
+// } 
+
+
+// // *{
+// //     1 
+// //    2 2
+// //   3 3 3
+// //  4 4 4 4
+// // 5 5 5 5 5 
+// // }
+
+
+public class numberpyramid {
+    public static void numberpy2(int n) {
+        // outerloop
+        for(int i=1; i<=n; i++) {
+            // for spaces
+            for(int j=1; j<=n-i; j++) {
+                System.out.print(" ");
+            }
+            // decending
+            for(int j=i; j>=1; j--) {
+                System.out.print(j);
+            }
+            // ascending
+            for(int j=2; j<=i;j++) {
+                System.out.print(j);
+            }
+            System.out.println();
+        }
+    }
+
+ 
+//     1
+//     212
+//    32123
+//   4321234
+//  543212345
+
+
+    public static void main(String[] args) {
+        numberpy2(5);
+    }
+} 
+
+
