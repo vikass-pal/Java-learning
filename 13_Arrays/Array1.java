7008b444f41efcac3694ebd3f033864ba2f3aa71
Vikas Pal <vikas.pal_2022@ltce.in>
2025-06-15 22:54:05 +0530

    Array started

---
+++ 13_Arrays/Array1.java
@@ -0,0 +1,7 @@
+public class Array1 {
+    public static void main(String[] args) {
+        int number[] = new int[50];
+        int morenum[] = {1,2,3};
+        String fruit[] = {"apple", "mango"};
+    }
+}
