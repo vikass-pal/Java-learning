6b0c87e0eef85b45674b79243af2032ae3154cde
Vikas Pal <vikas.pal_2022@ltce.in>
2025-06-21 22:57:46 +0530

    Bubble sort

--- BubbleSort.java
+++ BubbleSort.java
@@ -19,7 +19,7 @@ public class BubbleSort {
         System.out.println();
     }
     public static void main(String[] args) {
-        int arr[] = {5,4,1,3,2};
+        int arr[] = {5,4,1,3,2,8};
         bubbleSort(arr);
         printArr(arr);
     }
