a0114f1faffeb756ac77bbdcef932d989cd4465c
Vikas Pal <vikas.pal_2022@ltce.in>
2025-06-21 22:57:18 +0530

    Bubble sort

--- BubbleSort.java
+++ BubbleSort.java
@@ -12,7 +12,15 @@ public class BubbleSort {
             }
         }
     }
+    public static void printArr(int arr[]) {
+        for(int i=0;i<arr.length;i++) {
+            System.out.print(arr[i] + " ");
+        }
+        System.out.println();
+    }
     public static void main(String[] args) {
         int arr[] = {5,4,1,3,2};
+        bubbleSort(arr);
+        printArr(arr);
     }
 }
