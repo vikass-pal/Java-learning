2869a25dc3f3bf3653d8b9bbd4cdbc43f247aa60
Vikas Pal <vikas.pal_2022@ltce.in>
2025-06-21 22:46:52 +0530

    Bubble sort

---
+++ BubbleSort.java
@@ -0,0 +1,18 @@
+public class BubbleSort {
+
+    public static void bubbleSort(int arr[]) {
+        for(int turn = 0;turn<arr.length-1;turn++){
+            for(int j = 0; j<arr.length-1-turn;j++) {
+                if(arr[j] > arr[j+1]) {
+                    // swap
+                    int temp = arr[j];
+                    arr[j] = arr[j+1];
+                    arr[j+1] = temp;
+                }
+            }
+        }
+    }
+    public static void main(String[] args) {
+        int arr[] = {5,4,1,3,2};
+    }
+}
