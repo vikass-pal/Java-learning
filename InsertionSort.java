71194013db911b59a6ae6af891f7c0cadf495e69
Vikas Pal <vikas.pal_2022@ltce.in>
2025-06-23 23:10:37 +0530

    Insertion sort

---
+++ InsertionSort.java
@@ -0,0 +1,25 @@
+public class InsertionSort {
+    public static void insertionSort(int arr[]) {
+        for(int i=1;i<arr.length;i++) {
+            int curr = arr[i];
+            int prev = i-1;  
+            while(prev >= 0 && arr[prev] > curr) { //arr[prev] < curr) for decending
+                arr[prev+1] = arr[prev];
+                prev--;
+
+            }
+            arr[prev+1] = curr;
+        }  
+    }
+        public static void printArr(int arr[]) {
+        for(int i=0;i<arr.length;i++) {
+            System.out.print(arr[i] + " ");
+        }
+        System.out.println();
+        }
+    public static void main(String[] args) {
+        int[] arr = {6,3,9,4,1};
+       insertionSort(arr);
+       printArr(arr);
+    }
+}
