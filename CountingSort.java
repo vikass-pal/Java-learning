f9f3d03a0544bfbfed8dbc8df8428b1488f17e88
Vikas Pal <vikas.pal_2022@ltce.in>
2025-06-24 15:23:25 +0530

    CountingSort

---
+++ CountingSort.java
@@ -0,0 +1,35 @@
+public class CountingSort {
+    public static void countingSort(int arr[]) {
+        int largest = Integer.MIN_VALUE;
+        for(int i=0;i<arr.length;i++) {
+            largest = Math.max(largest, arr[i]);
+        }
+        int count[] = new int[largest+1];
+        for(int i=0;i<arr.length;i++) {
+            count[arr[i]]++;
+        }
+
+        // sorting
+        int j = 0;
+        for(int i =0;i<count.length;i++) {
+            while(count[i] > 0) {
+                arr[j] = i;
+                j++;
+                count[i]--;
+            }
+        }
+    }
+
+     public static void printArr(int arr[]) {
+        for(int i=0;i<arr.length;i++) {
+            System.out.print(arr[i] + " ");
+        }
+        System.out.println();
+        }
+
+    public static void main(String[] args) {
+        int[] arr = {64, 25, 12, 22, 11};
+        countingSort(arr);
+        printArr(arr);
+    }
+}
