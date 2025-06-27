e65f939e0eb0423ddc83f710811e37a3ac96d6b8
Vikas Pal <vikas.pal_2022@ltce.in>
2025-06-27 16:27:41 +0530

    Stingbuild Sting and buildleterstoUppercase

---
+++ Stingbuild.java
@@ -0,0 +1,9 @@
+public class Stingbuild {
+    public static void main(String[] args) {
+        StringBuilder sb = new StringBuilder("");
+        for(char ch='A';ch<='Z';ch++) {
+            sb.append(ch);
+        }
+        System.out.println(sb);
+    }
+}
