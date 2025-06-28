94bf2a6f9a3f73f86338e170bc2b22a904f4df0c
Vikas Pal <vikas.pal_2022@ltce.in>
2025-06-28 16:28:58 +0530

    ithbit

---
+++ ithbit.java
@@ -0,0 +1,15 @@
+public class ithbit {
+
+    public static int getithbit(int n , int i) {
+        int bitMask = 1<<i;
+        if((n & bitMask) == 0) {
+            return 0;
+        } else{
+            return 1;
+        }
+    }
+    public static void main(String[] args) {
+        System.out.println(getithbit(10, 1));
+        
+    }
+}
