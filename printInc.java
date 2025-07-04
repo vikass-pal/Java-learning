cf389ff48fbd56454f8efcdfcb506e9dede7eb4d
Vikas Pal <vikas.pal_2022@ltce.in>
2025-07-04 15:58:33 +0530

    printIncrease

--- printInc.java
+++ printInc.java
@@ -1,7 +1,7 @@
 public class printInc {
     public static void printIncrease(int n) {
         if(n == 1) {
-            System.out.print(n);
+            System.out.print(n + " ");
             return ;
         }
         printIncrease(n - 1);
