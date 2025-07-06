5ae6e63754e3f5f59cf583907e065d3f99594b6f
Vikas Pal <vikas.pal_2022@ltce.in>
2025-07-06 22:01:07 +0530

    friendPairingproblem

---
+++ friendPairingproblem.java
@@ -0,0 +1,21 @@
+public class friendPairingproblem {
+    public static int friendPair(int n) {
+        if(n == 0 || n == 1) {
+            return 1; // Base case: 0 or 1 friend can only be paired in one way
+        }
+        // choice
+        // single
+        int fnm1 = friendPair(n - 1);
+
+        // pair
+        int fnm2 = friendPair(n-2);
+        int pairways = (n-1) * fnm2;
+
+        // total ways
+        int totalWays = pairways + fnm1;
+        return totalWays;
+    }
+    public static void main(String[] args) {
+        System.out.println(friendPair(3));
+    }
+}
