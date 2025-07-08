e74621cbd2a14efd776996b61909df3823fcb826
Vikas Pal <vikas.pal_2022@ltce.in>
2025-07-08 15:21:57 +0530

    QuickSort

--- MergeSort.java
+++ MergeSort.java
@@ -1,6 +1,6 @@
 public class MergeSort {
 
-    public static void printArray(int[] arr) {
+    public static void printArray(int[] arr) {           
         for (int i = 0; i < arr.length; i++) {
             System.out.print(arr[i] + " ");
         }
