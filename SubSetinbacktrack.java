0ecda51f8a0bf7414eb8594b1d2ee7b5ce9fe31e
Vikas Pal <vikas.pal_2022@ltce.in>
2025-07-10 20:22:05 +0530

    Permutation

---
+++ SubSetinbacktrack.java
@@ -0,0 +1,22 @@
+public class SubSetinbacktrack {
+    public static void findSubSet(String str, String ans , int i) {
+        // base case
+        if(i == str.length()) {
+            if(ans.length() == 0) {
+                System.out.println("null");
+            } else {
+                System.out.println(ans);
+            }
+          return;
+        }
+        // yes choice
+        findSubSet(str, ans+str.charAt(i), i+1);
+
+        // no choice
+        findSubSet(str, ans, i+1);
+    }
+    public static void main(String[] args) {
+        String str = "abc";
+        findSubSet(str, "", 0);
+    }
+}
