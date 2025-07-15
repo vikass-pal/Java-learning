9528e394c45625a3dc4475a577b0d9efc1e303d7
Vikas Pal <vikas.pal_2022@ltce.in>
2025-07-15 22:45:15 +0530

    Add in middle and print

--- HeadTail.java
+++ HeadTail.java
@@ -33,8 +33,42 @@ public class HeadTail {
             head = tail = newNode;
             return;
         }
-        tail.next = tail;
+        tail.next = newNode;
         tail = newNode;
+    }
+     
+    // public void add(int idx, int data) {
+    //     Node newNode = new Node(data);
+    //     Node temp = head;
+    //     int i = 0;
+
+    //     while(i < idx -1) {
+    //         temp = temp.next;
+    //         i++;
+    //     }
+
+    //     newNode.next = temp.next;
+    //     temp.next = newNode;
+    // }
+
+    public void add(int idx, int data) {
+        if(idx == 0) {
+            addFirst(data);
+            return;
+        }
+        Node newNode = new Node(data);
+        Node temp = head;
+        int i = 0;
+
+        while(i < idx-1) {
+            temp = temp.next;
+            i++;
+        }
+
+        newNode.next = temp.next;
+        temp.next = newNode;
+
+
     }
 
     public void print() {
@@ -52,14 +86,15 @@ public class HeadTail {
 
     public static void main(String[] args) {
         HeadTail ll = new HeadTail();
-        ll.print();
+        
        ll.addFirst(2);
-       ll.print();
+       
        ll.addFirst(1);
-       ll.print();
+       
        ll.addLast(3);
-       ll.print();
+       
        ll.addLast(4);
+       ll.add(2, 9);
        ll.print();
 
         
