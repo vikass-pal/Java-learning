20d712b0523502dbe881472bb239e9774a141242
Vikas Pal <vikas.pal_2022@ltce.in>
2025-07-18 15:11:57 +0530

    LL loop Removal

--- removeCycle.java
+++ removeCycle.java
@@ -12,6 +12,23 @@ public class removeCycle {
     public static Node tail;
     public static int size;  
 
+    public static boolean isCycle() {
+        Node slow = head;
+        Node fast = head;
+
+        while(fast != null && fast.next != null) {
+            slow = slow.next;
+            fast = fast.next.next;
+
+            if(slow == fast) {
+                return true;
+            }
+        }
+        return false;
+
+
+        
+    }
     public static void removeCycle() {
         // detect cycle
         Node slow = head;
@@ -21,7 +38,7 @@ public class removeCycle {
             slow = slow.next;
             fast = fast.next.next;
             if(slow == fast) {
-                cycle = true;
+                cycle = true; 
                 break;
             }
         }
@@ -40,4 +57,16 @@ public class removeCycle {
         prev.next = null;
 
     }
+     public static void main(String[] args) {
+        head = new Node(1);
+        Node temp = new Node(2);
+        head.next = temp;
+        head.next.next = new Node(3);
+        head.next.next.next = temp;
+     System.out.println(isCycle());
+
+       removeCycle();
+       System.out.println(isCycle());
+    }
+
 }
