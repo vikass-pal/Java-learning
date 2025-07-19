b023165480986fdb3d67cd4d39343ead8818eabc
Vikas Pal <vikas.pal_2022@ltce.in>
2025-07-19 15:26:41 +0530

    LL MergeSort completed

--- MergeSortLL.java
+++ MergeSortLL.java
@@ -14,6 +14,19 @@ public class MergeSortLL {
     public static Node tail;
     public static int size;  
 
+      public void print() {
+        // if(head == null) {
+        //     System.out.println("Empty");
+        //     return;
+        // }
+       Node temp = head;
+       while(temp != null) {
+        System.out.print(temp.data + "->");
+        temp = temp.next;
+       } 
+       System.out.print("empty");
+    }
+
     private Node getMid(Node head) {
             Node slow = head;
             Node fast = head;
@@ -26,17 +39,40 @@ public class MergeSortLL {
     }
 
     private Node merge(Node head1, Node head2) {
-        Node mergeLL = new Node(-1);
+        Node mergedLL = new Node(-1);
         Node temp = mergedLL;
+         
+        while(head1 != null && head2 != null) {
+            if(head1.data <= head2.data) {
+                temp.next = head1;
+                head1 = head1.next;
+                temp = temp.next;
+            } else {
+                temp.next = head2;
+                head2 = head2.next;
+                temp = temp.next;
+            }
+        }
+        while(head1 != null) {
+            temp.next = head1;
+            head1 = head1.next;
+            temp = temp.next;
+        }
+        while(head2 != null) {
+            temp.next = head2;
+                head2 = head2.next;
+                temp = temp.next;
+        }
+        return mergedLL.next;
         
     }
 
     public Node mergeSort(Node head) {
-        if(head == null && head.next == null) {
+        if(head == null || head.next == null) {
             return head;
         }
         // find mid
-        Node mid = getMid();
+        Node mid = getMid(head);
         // left and right mergeSOrt
         Node rightHead = mid.next;
         mid.next = null;
@@ -49,7 +85,22 @@ public class MergeSortLL {
     }
 
      public static void main(String[] args) {
-          LinkedList<Integer> ll = new LinkedList<>();
+          MergeSortLL list = new MergeSortLL();
 
+          // Manually create the linked list using Node class
+          list.head = new Node(5);
+          list.head.next = new Node(4);
+          list.head.next.next = new Node(3);
+          list.head.next.next.next = new Node(2);
+          list.head.next.next.next.next = new Node(1);
+
+          System.out.println("Original Linked List:");
+          list.print();
+
+          // Sort the linked list using mergeSort
+          list.head = list.mergeSort(list.head);
+
+          System.out.println("\nSorted Linked List:");
+          list.print();
      }
 }
