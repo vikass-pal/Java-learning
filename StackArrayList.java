761da7fb840a470732eaed42631a1615cab5653e
Vikas Pal <vikas.pal_2022@ltce.in>
2025-07-22 21:11:18 +0530

    Stack started

---
+++ StackArrayList.java
@@ -0,0 +1,42 @@
+import java.util.*;
+public class StackArrayList {
+    static class StackB {
+        static ArrayList<Integer> list = new ArrayList<>();
+        public boolean isEmpty() {
+            return list.size() == 0;
+        }
+        // push
+        public void push(int data) {
+            list.add(data);
+        }
+        // pop
+        public int pop() {
+            if(isEmpty()) {
+                return -1;
+            }
+            int top = list.get(list.size() - 1);
+            list.remove(list.size() - 1);
+            return top;
+        }
+        // peek
+        public int peek() {
+            if(isEmpty()) {
+                return -1;
+            }
+            return list.get(list.size() -1);
+        }
+    } 
+      public static void main(String[] args) {
+        StackB s = new StackB();
+        s.push(1);
+        s.push(2);
+        s.push(3);
+
+        while(!s.isEmpty()) {
+          System.out.println(s.peek());
+          s.pop();
+
+        }
+        
+      }
+}
