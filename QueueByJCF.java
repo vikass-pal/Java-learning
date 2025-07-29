8d2e2b8a94ad5c41e94452d705941428a65b79f9
Vikas Pal <vikas.pal_2022@ltce.in>
2025-07-29 23:05:49 +0530

    Queue using JCF

--- QueueByJCF.java
+++ QueueByJCF.java
@@ -3,12 +3,15 @@ import java.util.*;
 public class QueueByJCF {
        public static void main(String[] args) {
        Queue<Integer> q = new LinkedList<>();
+    //    it can also be written as 
+    // Queue<Integer> q = new ArrayDeque<>();
         q.add(1);
-        q.add(2);
+        q.add(2); 
         q.add(3);
 
         while(!q.isEmpty()) {
-            System.out.println(q.remove());
+            System.out.println(q.peek());
+            q.remove();
         }
     }  
 }
