15d456b2765d42bd23244c43c41bbd613fca93be
Vikas Pal <vikas.pal_2022@ltce.in>
2025-08-01 19:59:44 +0530

    interleaveTwoHalves

--- DequeIntro.java
+++ DequeIntro.java
@@ -2,6 +2,7 @@ import java.util.*;
 public class DequeIntro {
     public static void main(String[] args) {
         // Deque<String> deque = new ArrayDeque<String>();
+        
         Deque<Integer> deque = new LinkedList<>();
         deque.addFirst(1);
         deque.addFirst(2);
@@ -12,6 +13,6 @@ public class DequeIntro {
         System.out.println("First"+deque.getFirst());
         System.out.println("Last"+deque.getLast());
          System.out.println(deque);
-         
+
     }
 }
