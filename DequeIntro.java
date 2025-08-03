fca7c7221d803a4ab128d91e4962e00920f35d5f
Vikas Pal <vikas.pal_2022@ltce.in>
2025-08-03 13:34:30 +0530

    interleaveTwoHalves

--- DequeIntro.java
+++ DequeIntro.java
@@ -13,6 +13,7 @@ public class DequeIntro {
         System.out.println("First"+deque.getFirst());
         System.out.println("Last"+deque.getLast());
          System.out.println(deque);
+         
 
     }
 }
