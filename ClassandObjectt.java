dfa9cfbb9f70bce8219ebc38fb68ee76d74bba5c
Vikas Pal <vikas.pal_2022@ltce.in>
2025-08-07 13:55:27 +0530

    Started OOPS

---
+++ ClassandObjectt.java
@@ -0,0 +1,36 @@
+public class ClassandObjectt {
+    public static void main(String[] args) {
+        // Creating an object of ClassA
+        Pen p1 = new Pen();
+        p1.setColor("Blue");
+        System.out.println("Pen color: " + p1.color);
+        p1.setTip(5);
+        System.out.println("Pen tip: " + p1.tip);
+        BankAccount myAcc = new BankAccount();
+        myAcc.username = "Vkas";
+        // myAcc.password = "12345"; // This line will cause an error if password is private
+        myAcc.setPassword("newPassword");
+    }
+    
+}
+
+class BankAccount {
+    public String username;
+    private String password;
+    public void setPassword(String newPassword) {
+        password = newPassword;
+    }
+}
+
+class Pen{
+    String color;
+    int tip;
+
+    void setColor(String newColor) {
+        color = newColor;
+    }
+
+    void setTip(int newTip) {
+        tip = newTip;
+    }
+}
