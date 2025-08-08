b855a03e9296b16729cd26b37b0280e94960e556
Vikas Pal <vikas.pal_2022@ltce.in>
2025-08-08 17:23:40 +0530

    Constructors,Shallow deep

--- Constructorr.java
+++ Constructorr.java
@@ -3,9 +3,10 @@ public class Constructorr {
         // Creating an instance of Student using the default constructor
         Student s = new Student();
         Student s1 = new Student("John");
+        s1.roll = 456;
         Student s2 = new Student(123);
         System.out.println("Student name: " + s1.name);
-    }
+    } 
 }
 
 class Student {
@@ -13,6 +14,10 @@ class Student {
     int roll;
     Student() {
         System.out.println("Constructor");
+    }
+    Student(Student s1) {
+        this.name = s1.name;
+        this.roll = s1.roll;
     }
 //  paramerterized constrictor
     Student(String name) {
