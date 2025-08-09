c966d020adec76a4cf8ebe87b3d53750115f569b
Vikas Pal <vikas.pal_2022@ltce.in>
2025-08-09 22:29:19 +0530

    SuperKeywordd

---
+++ SuperKeywordd.java
@@ -0,0 +1,19 @@
+public class SuperKeywordd {
+    public static void main(String[] args) {
+        Horse horse = new Horse();
+    }
+}
+
+class Animal {
+    Animal() {
+        System.out.println("Animal constructor called");
+    }
+}
+
+class Horse extends Animal {
+    Horse() {
+
+        super(); // Calls the constructor of the superclass Animal
+        System.out.println("Horse constructor called");
+    }
+}
