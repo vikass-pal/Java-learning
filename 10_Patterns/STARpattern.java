4b3632363522a7cc9aae1fbb4bab686a8cd80496
Vikas Pal <vikas.pal_2022@ltce.in>
2025-10-19 14:24:57 +0530

    MajorityElements

--- 10_Patterns/STARpattern.java
+++ 10_Patterns/STARpattern.java
@@ -1,11 +1,11 @@
 public class STARpattern {
     public static void main(String[] args) {
-         for(int line=1; line<=4; line++) {
-           for(int star=1; star<=line; star++) {
-             System.out.print("*");
-           }
-              System.out.println();
-         }
+        for(int i=1;i<4;i++) {
+          for(int j=1;j<=i;j++) {
+            System.out.println("*");
+          }
+          System.out.println();
+        }
     }
 }
    
--- MajorityElements.java
+++ MajorityElements.java
@@ -14,3 +14,44 @@ public class MajorityElements {
         }
     }
 }
+
+class Solution {
+    public List<Integer> majorityElement(int[] nums) {
+        HashMap<Integer,Integer>map = new HashMap<>();
+        for(int i=0;i<nums.length;i++) {
+            if(map.containsKey(nums[i])) {
+                map.put(nums[i],map.get(nums[i]) + 1) ;
+
+            } else{
+                map.put(nums[i],1);
+            }
+        }
+        Set<Integer> keySet = map.keySet();
+        for(Integer key : keySet) {
+            if(map.get(key) > nums.length/3 ) {
+                 System.out.println(key);
+            }
+        }
+        return -1;
+    }
+}
+// class Solution {
+//     public List<Integer> majorityElement(int[] nums) {
+//         HashMap<Integer,Integer>map = new HashMap<>();
+//         for(int i=0;i<nums.length;i++) {
+//             if(map.containsKey(nums[i])) {
+//                 map.put(nums[i],map.get(nums[i]) + 1) ;
+
+//             } else{
+//                 map.put(nums[i],1);
+//             }
+//         }
+//         Set<Integer> keySet = map.keySet();
+//         for(Integer key : keySet) {
+//             if(map.get(key) > nums.length/3 ) {
+//                  System.out.println(key);
+//             }
+//         }
+//         return -1;
+//     }
+// }
