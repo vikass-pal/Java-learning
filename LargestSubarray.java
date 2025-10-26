6c38df8e32d92fea65a6e67d073efafbeb855c32
Vikas Pal <vikas.pal_2022@ltce.in>
2025-10-26 20:11:13 +0530

    LargestSubarray

---
+++ LargestSubarray.java
@@ -0,0 +1,21 @@
+import java.util.*;
+public class LargestSubarray {
+    public static void main(String[] args) {
+        int arr[] = {15, -2, 2, -8, 1, 7, 10, 23};
+
+        HashMap<Integer,Integer> map = new HashMap<>();
+        int sum = 0;
+        int len = 0;
+
+        for(int j=0;j<arr.length;j++) {
+            sum += arr[j];
+
+            if(map.containsKey(sum)) {
+                len = Math.max(len, j - map.get(sum));
+            } else{
+                map.put(sum,j);
+            }
+        }
+        System.out.println("Length of largest subarray with 0 sum is: " + len);
+    }
+}
