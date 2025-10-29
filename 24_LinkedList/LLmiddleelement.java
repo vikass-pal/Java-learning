7cb0de5bde850ffb865d7b872c123c8e910f1e64
Vikas Pal <vikas.pal_2022@ltce.in>
2025-10-29 19:34:54 +0530

    LLLeet

--- 24_LinkedList/LLmiddleelement.java
+++ 24_LinkedList/LLmiddleelement.java
@@ -1,7 +1,6 @@
 public static boolean isCycle() {
         Node slow = head;
         Node fast = head;
-
         while(fast != null && fast.next != null) {
             slow = slow.next;
             fast = fast.next.next;
