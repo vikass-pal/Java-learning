7bdf1c6db2b908488c8256269c0ee70ded7dbcae
Vikas Pal <vikas.pal_2022@ltce.in>
2025-11-28 21:28:15 +0530

    searchInsert

---
+++ LongestPalindrome.java
@@ -0,0 +1,39 @@
+public class LongestPalindrome {
+   
+    public String longestPalindrome(String s) {
+        if (s.length() <= 1) {
+            return s;
+        }
+        String lps = "";
+        //   for odd
+        for (int i = 1; i < s.length(); i++) {
+            int low = i;
+            int high = i;
+
+            while (s.charAt(low) == s.charAt(high)) {
+                low--;
+                high++;
+                if (low == -1 || high == s.length())
+                    break;
+            }
+            String palindrome = s.substring(low + 1, high);
+            if (palindrome.length() > lps.length()) {
+                lps = palindrome;
+            }
+            // for even
+            low = i - 1;
+            high = i;
+            while (s.charAt(low) == s.charAt(high)) {
+                low--;
+                high++;
+                if (low == -1 || high == s.length())
+                    break;
+            }
+            palindrome = s.substring(low + 1, high);
+            if (palindrome.length() > lps.length()) {
+                lps = palindrome;
+            }
+        }
+        return lps;
+    }
+}
