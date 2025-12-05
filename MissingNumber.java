2613dc72345e7edc1bb46c1712895106fe630b1a
Vikas Pal <vikas.pal_2022@ltce.in>
2025-12-05 19:08:43 +0530

    MissingNumber

---
+++ MissingNumber.java
@@ -0,0 +1,13 @@
+public class MissingNumber {
+    
+    public int missingNumber(int[] nums) {
+       int n = nums.length;
+       int total = n * (n+1)/2;
+       int sum = 0;
+       for(int i=0;i<n;i++) {
+        sum += nums[i];
+       } 
+       return (int)(total - sum);
+    }
+}
+
