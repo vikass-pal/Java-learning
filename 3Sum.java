fb7da84807c67c180d90f8341828332e04a9e3ca
Vikas Pal <vikas.pal_2022@ltce.in>
2025-12-07 22:27:28 +0530

    3sum by hashset

---
+++ 3Sum.java
@@ -0,0 +1,24 @@
+public class 3Sum {
+
+    public List<List<Integer>> threeSum(int[] nums) {
+        HashSet<List<Integer>> set = new HashSet<>();
+        Arrays.sort(nums);
+        for (int i = 0; i < nums.length - 2; i++) {
+            int j = i + 1;
+            int k = nums.length - 1;
+            while (j < k) {
+                int sum = nums[i] + nums[j] + nums[k];
+                if (sum == 0) {
+                    set.add(Arrays.asList(nums[i], nums[j], nums[k]));
+                    j++;
+                    k--;
+                } else if (sum < 0) {
+                    j++;
+                } else {
+                    k--;
+                }
+            }
+        }
+        return new ArrayList<>(set);
+    }
+}
