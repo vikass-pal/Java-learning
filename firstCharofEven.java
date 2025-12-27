ecdd20373725b319aac9a6c748464aea91c91112
Vikas Pal <vikas.pal_2022@ltce.in>
2025-12-27 14:40:57 +0530

    firstCharofEven

---
+++ firstCharofEven.java
@@ -0,0 +1,22 @@
+import java.util.*;
+public class firstCharofEven {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        String str = sc.nextLine();
+        // String str = "aabbcde";
+        HashMap<Character,Integer> map = new HashMap<>();
+        for(int i=0;i<str.length();i++) {
+            char ch = str.charAt(i);
+            map.put(ch,map.getOrDefault(ch,0) + 1);
+        }
+        for(int i=0;i<str.length();i++) {
+            char c = str.charAt(i);
+            if(map.containsKey(c) && map.get(c) == 2) {
+                System.out.println(c);
+                break;
+            }
+            System.out.println("-1");
+        }
+       
+    }
+}
