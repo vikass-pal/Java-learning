7f23e267adf338fd2f0a1d576e188672b8dfa8ce
Vikas Pal <vikas.pal_2022@ltce.in>
2025-12-27 14:20:09 +0530

    leftEqualRight

---
+++ leftEqualRight.java
@@ -0,0 +1,32 @@
+import java.util.*;
+public class leftEqualRight {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        int nums[] = new int[n];
+        for(int i=0;i<n;i++) {
+            nums[i] = sc.nextInt();
+        }
+
+        int totalSum = 0;
+        for(int i=0;i<n;i++) {
+            totalSum +=nums[i];
+        }
+        boolean possible = false;
+        int left = 0;
+        int sum = totalSum/2;
+        for(int i=0;i<n;i++) {
+            int right = totalSum - left -nums[i];
+            if(left == right) {
+                possible = true;
+                break;
+            } 
+            left+=nums[i];
+        }
+        if(possible) {
+            System.out.println("YES");
+        } else {
+            System.out.println("NO");
+        }
+    }
+}
