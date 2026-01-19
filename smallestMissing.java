16766b2635b7f83be603a1c85e8d75c90a1c5e0f
Vikas Pal <vikas.pal_2022@ltce.in>
2026-01-19 22:31:45 +0530

    SmallestMissing

--- smallestMissing.java
+++ smallestMissing.java
@@ -25,9 +25,6 @@ class Solution {
 // Given an unsorted integer array nums. Return the smallest positive integer that is not present in nums.
 
 // You must implement an algorithm that runs in O(n) time and uses O(1) auxiliary space.
-
- 
-
 // Example 1:
 
 // Input: nums = [1,2,0]
