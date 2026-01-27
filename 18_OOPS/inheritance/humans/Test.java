9d6a364da022eed053964cafea2d1ac1d4b44c16
Vikas Pal <vikas.pal_2022@ltce.in>
2026-01-27 21:05:38 +0530

    inheritance super

---
+++ 18_OOPS/inheritance/humans/Test.java
@@ -0,0 +1,10 @@
+public class Test{
+    public static void main(String[] args) {
+        child c = new child();
+
+    }
+
+    // super se kya hota hai;
+   // super se parameterized constructor call kara sakte hai;
+   //compile time pe memory allocate nahi hoti runtime pe hoti hai;
+}
