4fa6ef335dd56ae952ef49f79c9818a890972e8a
Vikas Pal <vikas.pal_2022@ltce.in>
2026-02-25 23:26:26 +0530

    fruit/sliding

---
+++ fruitInBasket.java
@@ -0,0 +1,27 @@
+import java.util.HashMap;
+
+public class fruitInBasket {
+    class Solution {
+    public int totalFruit(int[] fruits) {
+        int left = 0, right = 0, maxlen = 0;
+        HashMap<Integer, Integer> map = new HashMap<>();
+        while (right < fruits.length) {
+            map.put(fruits[right],
+                    map.getOrDefault(fruits[right], 0) + 1);
+            while (map.size() > 2) {
+                map.put(fruits[left], map.get(fruits[left]) - 1);
+
+                if (map.get(fruits[left]) == 0) {
+                    map.remove(fruits[left]);
+                }
+                left++;
+            }
+            maxlen = Math.max(maxlen, right - left + 1);
+            right++;
+        }
+        return maxlen;
+    }
+}
+
+
+}
