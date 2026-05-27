ba3a3abc5114763fc1df72bf1ff8d04638aa0724
Vikas Pal <vikas.pal_2022@ltce.in>
2026-05-27 12:59:07 +0530

    abs

--- 17_BitManupulation/ithbit.java
+++ 17_BitManupulation/ithbit.java
@@ -8,7 +8,7 @@ public class ithbit {
             return 1;
         }
     }
-    public static void main(String[] args) {
+public static void main(String[] args) {
         System.out.println(getithbit(10, 1)); 
     }
 }
--- 18_OOPS/Abstraction/Animal.java
+++ 18_OOPS/Abstraction/Animal.java
@@ -3,6 +3,7 @@ package Abstraction;
 public abstract class Animal {
     
     public abstract void sayHello();  //this is abstract method;
+    public abstract void sayHii();
 
     public void sleep() {
         System.out.println("zzzz");    //this is concrete method
