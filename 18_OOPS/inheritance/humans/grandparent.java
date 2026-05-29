0bd013060c0eb3a5e11037ea9fac2e740df7e40d
Vikas Pal <vikas.pal_2022@ltce.in>
2026-05-29 17:49:38 +0530

    in

--- 18_OOPS/inheritance/humans/grandparent.java
+++ 18_OOPS/inheritance/humans/grandparent.java
@@ -4,6 +4,7 @@ public class grandparent{
     public grandparent(String name,int age) {
         this.age = age;
         this.name=name;
+        
         System.out.println("grand is called");
     }
 }
