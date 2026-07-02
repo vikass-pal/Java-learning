e83d1cf29165646dff56090c6590d6dc9f4cfc09
Vikas Pal <vikas.pal_2022@ltce.in>
2026-07-02 22:37:47 +0530

    leet

--- tree/BuildTree.java
+++ tree/BuildTree.java
@@ -1,4 +1,5 @@
-public class BuildTree{
+public class BuildTree {
+
     static class Node {
         int data;
         Node left;
@@ -7,37 +8,46 @@ public class BuildTree{
         Node(int data) {
             this.data = data;
             this.left = null;
-            this.right=null;
+            this.right = null;
         }
     }
-    static class binarytree {
-        static int idx = -1;
-        public static Node buildtree(int nodes[]) {
+
+    // Builds a binary tree from an array in preorder format where -1 means "null".
+    static class BinaryTree {
+        private static int idx;
+
+        public static Node buildTree(int[] nodes) {
+            idx = -1;
+            return buildTreeHelper(nodes);
+        }
+
+        private static Node buildTreeHelper(int[] nodes) {
             idx++;
-            if(idx == -1) {
+            if (idx >= nodes.length || nodes[idx] == -1) {
                 return null;
             }
+
             Node newNode = new Node(nodes[idx]);
-            newNode.left = buildtree(nodes);
-            newNode.right = buildtree(nodes);
-
-            return newNode; 
-
+            newNode.left = buildTreeHelper(nodes);
+            newNode.right = buildTreeHelper(nodes);
+            return newNode;
         }
-        public static void preorder(root) {
-            if(root == null) {
+
+        public static void preorder(Node root) {
+            if (root == null) {
                 return;
             }
-            System.out.println(root.data+" ");
+            System.out.print(root.data + " ");
             preorder(root.left);
             preorder(root.right);
         }
     }
 
-    public static void main(String args[]) {
-        int node[] = {1,2,4,-1,-1,5,-1,-1,3,-1,6,-1,-1};
-        binarytree tree = new binarytree();
-        Node root = tree.buildTree(nodes);
-        tree.preorder(root);
+    public static void main(String[] args) {
+        int[] nodes = {1, 2, 4, -1, -1, 5, -1, -1, 3, -1, 6, -1, -1};
+
+        Node root = BinaryTree.buildTree(nodes);
+        BinaryTree.preorder(root);
     }
-}  
+}
+
