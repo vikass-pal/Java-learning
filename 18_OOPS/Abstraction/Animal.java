a8212c20a125df478997bd9a8bcedc63f479d80f
Vikas Pal <vikas.pal_2022@ltce.in>
2026-08-09 20:43:21 +0530

    leet

--- 18_OOPS/Abstraction/Animal.java
+++ 18_OOPS/Abstraction/Animal.java
@@ -5,6 +5,7 @@ public abstract class Animal {
     public abstract void sayHello();  //this is abstract method;
     public abstract void sayHii();
 
+    
     public void sleep() {
         System.out.println("zzzz");    //this is concrete method
     }
